2bb5dd88a0d34d62dbc41233ddd1a1476b1e75d7
vinadwii <143859956+vinadwii@users.noreply.github.com>
2023-09-02 11:00:10 +0700

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,8 @@
+persegi_panjang = int(input("masukkan panjang"))
+nama2 = int(input("masukkan lebar"))
+nama3 = int (input("masukkan nama"))
+nama4 = float (input("masukkan angka"))
+hitung = nama3 + nama4
+print (nama3)
+print (nama4)
+print (hitung)
